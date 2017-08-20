8bbefc19c0fccc11ce4059923e4838ad5a4a0f96
KHom <dermuelleimer@live.de>
2017-08-20 15:47:01 +0200

    edit page updated
    Porblem solved. all entries are now updateable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
-      protected
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
+   end
 
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
-      end
+  # If you have extra params to permit, append them to the sanitizer.
+   def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
+   end
+
+
 
   # Confirms a logged-in user.
   def signed_in_user #method that defines that unless the answer of user_signed_in? is true. its true
