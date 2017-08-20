da8e8846d7c5c3ad24defc7caac9e43f76c1bead
KHom <dermuelleimer@live.de>
2017-08-20 03:34:33 +0200

    User model fix
    Problem solved that users cant have custom attributs.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+      protected
+
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
+      end
+
   # Confirms a logged-in user.
   def signed_in_user #method that defines that unless the answer of user_signed_in? is true. its true
     unless user_signed_in? # Method defined by Devise
