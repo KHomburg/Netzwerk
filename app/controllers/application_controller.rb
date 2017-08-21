041b2f26ba069f2efdaacb0806aa41cc9c05a9df
KHom <dermuelleimer@live.de>
2017-08-21 04:41:58 +0200

    adjustment in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :firstname, :date_of_birth, :aktiv_seit, :bund, :bio])
+  end
+
 
   # Confirms a logged-in user.
   def signed_in_user #method that defines that unless the answer of user_signed_in? is true. its true
